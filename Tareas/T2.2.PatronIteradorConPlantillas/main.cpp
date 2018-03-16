2f57ba463e7306bc4a040a883583f97eb6eae230
eugenioLeal <eugenio.leal.333@gmail.com>
2018-03-15 23:14:37 -0600

    Iterador Con Plantillas 1(falta acabar)

--- Tareas/T2.2.PatronIteradorConPlantillas/main.cpp
+++ Tareas/T2.2.PatronIteradorConPlantillas/main.cpp
@@ -4,28 +4,42 @@ template <class T>
 class Collection {
 public:
   virtual void add(T element) = 0;
-  virtual int at(T position) = 0;
-  virtual Iterator* getIterator() = 0;
-  virtual int getNumElements() = 0;
+  virtual T at(T position) = 0;
+  // virtual Iterator<Array>* getIterator() = 0;
+  virtual T getNumElements() = 0;
 };
 
 template <class T>
 class Iterator {
 private:
-  Collection c;
+  Collection<T> c;
 public:
   Iterator(Collection<T> cc) {
     c = cc;
   }
   bool hasNext() {
-    if() {
+    if(1) {
 
     }
     return true;
   }
+  int next() {
+
+  }
 };
 
 int main(int argc, char const *argv[]) {
+  // Create a collection of integers
+  Collection<int>* int_collection = new 
+
+  // Create a Collection of floats
+
+  // Create an iterator for an array
+  Iterator<Array>* i = int_collection->getIterator();
+  while( i->hasNext() ) {
+    cout << i->next() << " ";
+  }
+  cout << endl;
 
   return 0;
 }
